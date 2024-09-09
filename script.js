ade804df1dcc540391c0ed3169f0c804c0a9db3e
Luis Cardona <cardona-luis@outlook.com>
2024-09-09 11:22:42 -0500

    BreakfastMenu

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const MenuHandler = e => {
 
     // Define a mapping of menu item texts to their corresponding menu element IDs
     const menuMapping = {
-        "Breakfast": "breakfastMenuList",
+        "Breakfast: 7 am - 11 am": "breakfastMenuList",
         "Pizza": "pizzasMenu",
         "Sandwiches": "sandwichesMenu",
         "Appetizers": "appetizersMenu",
@@ -49,29 +49,75 @@ const BreakfastIemGenerator = e => {
     const breakfastMenu = {
         "Good Morning": {
             id: "breakfast1",
-            description: "1 egg, 2 bacon strips, 2 sausage links, hash browns and 2 pancakes.",
-            price: "$5.00"
+            description: "1 egg, 2 slices of bacon or sausage links or ham, with hash browns or country potatoes.",
+            price: "$8.00"
         },
-        "Healthy Start": {
+        "Biscuits & Gravy": {
             id: "breakfast2",
             description: "Egg white omelette with spinach, mushrooms, and tomatoes. Served with fresh fruit.",
-            price: "$6.50"
+            price: "$10.00"
         },
-        "Classic Pancakes": {
+        "Country Fried Stake & Eggs": {
             id: "breakfast3",
             description: "Three fluffy pancakes served with butter and maple syrup.",
-            price: "$4.50"
+            price: "$15.00"
         },
-        "French Toast": {
+        "Joe's Skillet": {
             id: "breakfast4",
-            description: "Thick slices of bread dipped in a mixture of eggs, milk, and cinnamon, then cooked until golden brown.",
-            price: "$5.50"
+            description: "Spinach, mushrooms, sausage or bacon with red onions, tomatoes scramble topped with cheese served on a bed of country potatoes on a sizzling skillet.",
+            price: "$13.00"
+        },
+        "Eggs Benedict": {
+            id: "breakfast5",
+            description: "English muffin topped with a poached egg, ham and hollandaise sauce.",
+            price: "$12.00"
+        },
+
+        "Breakfast Sandwich": {
+            id: "breakfast6",
+            description: "2 scrambled eggs with bacon, sausage, or ham. Serve on a english muffin.",
+            price: "$6.00"
         },
         "Breakfast Burrito": {
-            id: "breakfast5",
-            description: "Scrambled eggs, cheese, bacon, and salsa wrapped in a warm tortilla.",
-            price: "$6.00"
-        }
+            id: "breakfast7",
+            description: "Scrambled eggs, cheese, bacon, sausage, or ham with country potatoes cheddar jack colby cheese wrapped in a flour tortilla.",
+            price: "$8.00"
+        },
+        "Bacon Omelette": {
+            id: "breakfast8",
+            description: "Loaded with crispy diced bacon and cheddar jack cheese. Served with country potatoes or hash browns.",
+            price: "$12.00"
+        },
+        "Veggie Omelette": {
+            id: "breakfast9",
+            description: "Mushrooms, Tomatoes, Onions, Spinach and Cheddar Jack Colby Cheeses Blend. Served with country potatoes or hash browns.",
+            price: "$12.00"
+        },
+        "Ham & Cheese Omelette": {
+            id: "breakfast10",
+            description: "Filled with ham and melted cheese. Served with country potatoes or hash browns.",
+            price: "$12.00"
+        },
+        "Denver Omelette": {
+            id: "breakfast11",
+            description: "Diced Ham, bell peppers, onions, and cheese.  Served with country potatoes or hash browns.",
+            price: "$12.00"
+        },
+        "Buttermilk Pancakes": {
+            id: "breakfast12",
+            description: "Pancakes served with butter and maple syrup.",
+            price: "(2) $7.00 (4) $10.00"
+        },
+        "French Toast": {
+            id: "breakfast13",
+            description: "Thick sliced bread dipped in our special batter and grilled to a golden brown.",
+            price: "$8.00"
+        },
+        "Waffle": {
+            id: "breakfast14",
+            description: "Golden brown waffle served with butter and maple syrup.",
+            price: "$8.00"
+        },
     };
 
     const breakfastMenuList = document.getElementById("breakfastMenuList");
